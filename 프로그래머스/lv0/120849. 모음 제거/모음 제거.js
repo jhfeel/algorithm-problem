77a7df2596ab903e622dad230b021db99b4be517
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2023-03-15 19:48:02 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,12 +1,12 @@
 function solution(my_string) {
-    const aeiou = ['a','e','i','o','u'];
-    let newStr = [];
-    for(let i=0;i<my_string.length;i++){
-        if(aeiou.includes(my_string[i])===false){
-            newStr.push(my_string[i]);   
-        }
-        
-    }
+    // const aeiou = ['a','e','i','o','u'];
+    // let newStr = [];
+    // for(let i=0;i<my_string.length;i++){
+    //     if(aeiou.includes(my_string[i])===false){
+    //         newStr.push(my_string[i]);   
+    //     }
+    // }
+    // return newStr.join('');
     
-    return newStr.join('');
+    return my_string.split('a').join('').split('e').join('').split('i').join('').split('o').join('').split('u').join('');
 }
