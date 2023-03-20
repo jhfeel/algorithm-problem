06f2b2b8e2c6bd8dc6d6d76afcaa5e112b3f1d5a
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2023-03-20 09:07:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    my_string = [...my_string];    
+    let tmp;
+    tmp = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = tmp;
+    
+    return my_string.join('');
+}
