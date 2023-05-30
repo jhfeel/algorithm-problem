4cb4be2f227741a8e1bcaca192b3e662adfd00f9
J.H. Kim <127109684+jhfeel@users.noreply.github.com>
2023-05-30 11:06:00 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let data = input.map(Number);
+let mySet = new Set(); 
+
+for (let i = 0; i < 10; i++) {
+    mySet.add(data[i] % 42); 
+}
+
+console.log(mySet.size);
