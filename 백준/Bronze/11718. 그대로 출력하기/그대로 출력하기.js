9266533a706e059ad0a5002ccc4cbe6fe9847c30
J.H. Kim <127109684+jhfeel@users.noreply.github.com>
2024-06-30 20:52:45 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,5 @@
+const fileFath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const fs = require("fs");
+let input = fs.readFileSync(fileFath).toString().trim().split("\n");
+
+console.log(input.join("\n"));
